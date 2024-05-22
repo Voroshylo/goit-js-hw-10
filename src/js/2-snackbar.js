ccd7d00c830c37af2efce76303b0b50af74f0e95
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-05-22 23:44:43 +0300

    1

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,64 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const delayInput = form.querySelector('input[name="delay"]');
+  const stateInput = form.querySelector('input[name="state"]:checked');
+  const delay = parseInt(delayInput.value);
+  const state = stateInput.value;
+  
+  if (isNaN(delay)) {
+    showMessage('Please enter a valid delay value.');
+    return;
+  }
+  try {
+    const result = await generatePromise(delay, state);
+    if (state === 'fulfilled') {
+      showSuccessMessage(result);
+    } else {
+      showErrorMessage(result);
+    }
+} catch (error) {
+  showErrorMessage('An error occurred: ${error.message}');
+    
+  }
+});
+
+function generatePromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
+function showSuccessMessage(delay) {
+  iziToast.success({
+    title: 'Fulfilled promise',
+    message: `✅ Fulfilled promise in ${delay}ms`,
+    position: 'topRight'
+  });
+}
+
+function showErrorMessage(delay) {
+  iziToast.error({
+    title: 'Rejected promise',
+    message: `❌ Rejected promise in ${delay}ms`,
+    position: 'topRight'
+  });
+}
+
+function showMessage(message) {
+  iziToast.warning({
+    title: 'Warning',
+    message: message,
+    position: 'topRight'
+  });
+}
